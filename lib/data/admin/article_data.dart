999794b34b97e0dcbf9bfb1f242fbbe9a958fbcd
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-05-27 04:00:17 +0800

    [refactor] announcements model and data

---
+++ lib/data/admin/article_data.dart
@@ -0,0 +1,16 @@
+import '../../models/admin/article_model.dart';
+
+final List<Article> announcements = [
+  Article(
+    title: 'Statement on red-tagging of PLM',
+    description: 'The Pamantasan ng Lungsod ng Maynila (PLM) was once again the unscrupulous allegations made by a high official of the military...',
+  ),
+  Article(
+    title: '2021 New Year Message from the University President',
+    description: 'No one had a 2020 vision of what this year that is about to end would bring. Our experiences have been difficult but we have a lot...',
+  ),
+  Article(
+    title: 'Preparations against COVID-19 Pandemic',
+    description: 'Dear PLM Community, The COVID-19 pandemic has brought difficult times for everybody...',
+  ),
+];
---
+++ lib/models/admin/article_model.dart
@@ -0,0 +1,7 @@
+class Article {
+  final String title;
+  final String description;
+  final String? imagePath;
+
+  Article({required this.title, required this.description, this.imagePath});
+}
