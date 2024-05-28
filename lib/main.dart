6ec569e13dbca23ab12852161bab2c5f901a5be5
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-05-28 19:09:28 +0800

    [refactor] enrollment page set for integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:mysample/screens/admin/onboarding/onboarding_admin.dart';
-import 'package:mysample/screens/faculty/onboarding/onboarding_faculty.dart';
+// import 'package:mysample/screens/faculty/onboarding/onboarding_faculty.dart';
 // import 'bottom_navigation.dart';
 import 'package:mysample/utils/app_styles.dart';
 // import 'package:mysample/widgets/base_layout.dart';
+import 'package:flutter/services.dart';
+import 'package:sizer/sizer.dart';
 
-void main() => runApp(const MyApp());
+var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        canvasColor: AppTheme.baseBlue,
-      ),
-      home: const OnBoardingAdmin(),
-    );
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          canvasColor: AppTheme.baseBlue,
+        ),
+        home: const SafeArea(child: OnBoardingAdmin()),
+      );
+    });
   }
 }
