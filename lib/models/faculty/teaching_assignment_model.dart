079d5f814f112ba0c7baed15dec7d41553982911
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-05-29 00:43:01 +0800

    [feature] teaching assignment model

---
+++ lib/models/faculty/teaching_assignment_model.dart
@@ -0,0 +1,21 @@
+class TeachingAssignment {
+  final String subjectCodeAndSection;
+  final String subjectTitle;
+  final String units;
+  final String schedule;
+  final String noStudents;
+  final String creditedUnits;
+  final String college;
+  final String typeOfLoad;
+
+  TeachingAssignment({
+    required this.subjectCodeAndSection,
+    required this.subjectTitle,
+    required this.units,
+    required this.schedule,
+    required this.noStudents,
+    required this.creditedUnits,
+    required this.college,
+    required this.typeOfLoad,
+  });
+}
