09da6f2efff4ce90eab954691e745c9842a730fb
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-05-29 00:45:34 +0800

    [feature] teaching assignment data

---
+++ lib/data/faculty/teaching_assignment_data.dart
@@ -0,0 +1,34 @@
+import '../../models/faculty/teaching_assignment_model.dart';
+
+final List<TeachingAssignment> teachingAssignments = [
+  TeachingAssignment(
+    subjectCodeAndSection: 'CFS 0001',
+    subjectTitle: 'OOP (LEC)',
+    units: '2',
+    schedule: 'M 12:30p-1:30p F2F COMP LAB 3',
+    noStudents: '40',
+    creditedUnits: '2',
+    college: 'CSITM',
+    typeOfLoad: '',
+  ),
+  TeachingAssignment(
+    subjectCodeAndSection: 'CFS 0002',
+    subjectTitle: 'OOP (LAB)',
+    units: '1',
+    schedule: 'T 12:30p-1:30p F2F COMP LAB 3',
+    noStudents: '39',
+    creditedUnits: '3',
+    college: 'CSITM',
+    typeOfLoad: '',
+  ),
+  TeachingAssignment(
+    subjectCodeAndSection: 'CFS 0003',
+    subjectTitle: 'OOP (LEC)',
+    units: '2',
+    schedule: 'W 12:30p-1:30p F2F COMP LAB 3',
+    noStudents: '32',
+    creditedUnits: '2',
+    college: 'CSITM',
+    typeOfLoad: '',
+  ),
+];
