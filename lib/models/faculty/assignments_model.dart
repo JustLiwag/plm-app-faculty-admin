ad98ba3517c61502d6b4603352c113d792e9683e
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-05-28 23:00:25 +0800

    [feature] current class assignment

---
+++ lib/models/faculty/assignments_model.dart
@@ -0,0 +1,15 @@
+class Assignment {
+  final String classCode;
+  final String courseCodeSection;
+  final String courseTitle;
+  final String classSchedule;
+  final String credits;
+
+  Assignment({
+    required this.classCode,
+    required this.courseCodeSection,
+    required this.courseTitle,
+    required this.classSchedule,
+    required this.credits,
+  });
+}
