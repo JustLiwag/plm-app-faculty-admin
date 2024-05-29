6c288e8d2f3720adadb20e8c94630005ae48ddca
mebcrtz <81579471+mebcrtz@users.noreply.github.com>
2024-05-29 17:41:25 +0800

    [misc] data fix

--- lib/data/faculty/subject_data.dart
+++ lib/data/faculty/subject_data.dart
@@ -1,17 +1,16 @@
 import 'package:mysample/models/faculty/student_model.dart';
 import 'package:mysample/models/faculty/subject_model.dart';
 
-// Define student lists
 List<Student> studentsForSubject1 = [
   Student(
-    studentNumber: '123456',
-    grade: 'A',
+    studentNumber: '202101350',
+    grade: '1.00',
     studentName: 'John Doe',
     college: 'College of Engineering',
   ),
   Student(
-    studentNumber: '234567',
-    grade: 'B',
+    studentNumber: '202101351',
+    grade: '1.25',
     studentName: 'Jane Smith',
     college: 'College of Engineering',
   ),
@@ -19,14 +18,14 @@ List<Student> studentsForSubject1 = [
 
 List<Student> studentsForSubject2 = [
   Student(
-    studentNumber: '345678',
-    grade: 'C',
+    studentNumber: '202101352',
+    grade: '1.25',
     studentName: 'Alice Johnson',
     college: 'College of Engineering',
   ),
   Student(
-    studentNumber: '456789',
-    grade: 'B+',
+    studentNumber: '202101353',
+    grade: '1.50',
     studentName: 'Bob Brown',
     college: 'College of Engineering',
   ),
@@ -34,14 +33,14 @@ List<Student> studentsForSubject2 = [
 
 List<Student> studentsForSubject3 = [
   Student(
-    studentNumber: '567890',
-    grade: 'A-',
+    studentNumber: '202101354',
+    grade: '1.25',
     studentName: 'Charlie Green',
     college: 'College of Science',
   ),
   Student(
-    studentNumber: '678901',
-    grade: 'B-',
+    studentNumber: '202101355',
+    grade: '1.75',
     studentName: 'Dana White',
     college: 'College of Science',
   ),
@@ -49,14 +48,14 @@ List<Student> studentsForSubject3 = [
 
 List<Student> studentsForSubject4 = [
   Student(
-    studentNumber: '789012',
-    grade: 'B+',
+    studentNumber: '202101356',
+    grade: '1.25',
     studentName: 'Evan Blue',
     college: 'College of Arts',
   ),
   Student(
-    studentNumber: '890123',
-    grade: 'A',
+    studentNumber: '202101357',
+    grade: '1.25',
     studentName: 'Fiona Red',
     college: 'College of Arts',
   ),
@@ -72,19 +71,19 @@ List<Subject> subjectList = [
   Subject(
     courseCode: 'ICC 0106-2',
     courseTitle: 'Application Development and Emerging Technologies (Lecture)',
-    schedule: 'M 9:30a-12:30p F2F GC 201',
+    schedule: 'W 9:30a-12:30p F2F GC 201',
     enrolledStudents: studentsForSubject2,
   ),
   Subject(
     courseCode: 'ICC 0106-3',
     courseTitle: 'Application Development and Emerging Technologies (Lecture)',
-    schedule: 'M 9:30a-12:30p F2F GC 201',
+    schedule: 'F 9:30a-12:30p F2F GC 201',
     enrolledStudents: studentsForSubject3,
   ),
   Subject(
     courseCode: 'ICC 0106-1.3',
     courseTitle: 'Application Development and Emerging Technologies (Laboratory)',
-    schedule: 'M 9:30a-12:30p F2F GC 201',
+    schedule: 'S 9:30a-12:30p F2F GC 201',
     enrolledStudents: studentsForSubject4,
   ),
 ];
